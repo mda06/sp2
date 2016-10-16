4008d6ede0a03b561852a947836974249a950d25
mda06 <michaelvm171@msn.com>
2016-10-16 21:47:04 +0200

    Remove the sysout of dev branch because it was on the main brnach

--- Software Project 2/src/com/school/project/Main.java
+++ Software Project 2/src/com/school/project/Main.java
@@ -5,7 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("We are the best");
 		System.out.println("JSON.jar is now in the main branch!");
-		System.out.println("This is our dev branch");
 	}
 
 }
