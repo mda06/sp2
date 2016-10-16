9746b9805bc5d2f238937800bd925ba6d4bf8b3c
mda06 <michaelvm171@msn.com>
2016-10-16 22:01:54 +0200

    Better welcome sysout

--- Software Project 2/src/com/school/project/Main.java
+++ Software Project 2/src/com/school/project/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("We are the best");
 		System.out.println("JSON.jar is now in the main branch!");
-		System.out.println("We are now in the dev branch");
+		System.out.println("We are now in the dev branch and not in the main");
 	}
 
 }
