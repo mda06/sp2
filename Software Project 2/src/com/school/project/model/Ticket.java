e098948984a02cdc7554636b7095b144105bdc5d
naits <naits@10.3.208.205>
2016-10-17 11:55:20 +0200

    Klasse Ticket aangemaakt met membervariabelen

---
+++ Software Project 2/src/com/school/project/model/Ticket.java
@@ -0,0 +1,12 @@
+package com.school.project.model;
+
+public class Ticket {
+	private int id;
+	private String name;
+	private String description;
+	private double price;
+	private int validityPeriod;
+	private boolean hasFixedRoute;
+	
+	
+}
