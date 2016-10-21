a722cc2eff50bf86ee486fc08db15d2a8cca8b7b
Willem-Jan Pattyn <Willem-Jan Pattyn@192.168.1.6>
2016-10-21 14:20:39 +0200

    Improved test cases TestAddress and TestLostItem

--- Software Project 2/src/com/school/project/testing/TestLostItem.java
+++ Software Project 2/src/com/school/project/testing/TestLostItem.java
@@ -7,35 +7,95 @@ import org.junit.Test;
 import com.school.project.model.LostItem;
 
 public class TestLostItem {
-
-	int id = 1;
-	String type = "phone";
-	String description = "iPhone 6 with yellow case";
-	String location = "Bruxelles-Midi";
-	boolean pickedUp = false;
-	boolean archived = false;
 	
-	LostItem li1 = new LostItem(id, type, description, location, pickedUp, archived);
-	LostItem li2 = new LostItem(1, "phone", "iPhone 6 with yellow case", "Bruxelles-Midi", false, false);
+	int check = 1; //1 = true, 0 = false
 	
 	@Test
 	public void testConstructor(){
-		assertEquals(id, li1.getId());
-		assertEquals(type, li1.getType());
-		assertEquals(description, li1.getDescription());
-		assertEquals(location, li1.getLocation());
-		assertEquals(pickedUp, li1.isPickedUp());
-		assertEquals(archived, li1.isArchived());
+		int id = 1;
+		String type = "phone";
+		String description = "iPhone 6 with yellow case";
+		String location = "Bruxelles-Midi";
+		boolean pickedUp = false;
+		boolean archived = false;
+		
+		LostItem li1 = new LostItem(id, type, description, location, pickedUp, archived);
+		
+		if(check == 1){
+			assertEquals(id, li1.getId());
+			assertEquals(type, li1.getType());
+			assertEquals(description, li1.getDescription());
+			assertEquals(location, li1.getLocation());
+			assertEquals(pickedUp, li1.isPickedUp());
+			assertEquals(archived, li1.isArchived());
+		}
+		else if(check == 0){
+			li1.setId(1);
+			li1.setType("phone");
+			li1.setDescription("Samsung Galaxy 7 with blue case");
+			li1.setLocation("Jette");
+			li1.setPickedUp(true);
+			li1.setArchived(true);
+			
+			assertEquals(id, li1.getId());
+			assertEquals(type, li1.getType());
+			assertEquals(description, li1.getDescription());
+			assertEquals(location, li1.getLocation());
+			assertEquals(pickedUp, li1.isPickedUp());
+			assertEquals(archived, li1.isArchived());
+		}
 	}
 	
 	@Test
-	public void testEquals(){				
-		assertEquals(true, li1.equals(li2));
+	public void testEquals(){	
+		int id = 1;
+		String type = "phone";
+		String description = "iPhone 6 with yellow case";
+		String location = "Bruxelles-Midi";
+		boolean pickedUp = false;
+		boolean archived = false;
+		
+		LostItem li1 = new LostItem(id, type, description, location, pickedUp, archived);
+		LostItem li2 = new LostItem(1, "phone", "iPhone 6 with yellow case", "Bruxelles-Midi", false, false);
+		
+		if(check == 1){
+			assertEquals(true, li1.equals(li2));
+		}
+		else if(check == 0){
+			li1.setId(1);
+			li1.setType("accessoires");
+			li1.setDescription("Battery charger");
+			li1.setLocation("Vilvoorde");
+			li1.setPickedUp(true);
+			li1.setArchived(true);
+			
+			assertEquals(true, li1.equals(li2));
+		}
 	}
 	
 	@Test
 	public void testHashCode(){
-		assertEquals(li1.hashCode(), li2.hashCode());
+		int id = 1;
+		String type = "phone";
+		String description = "iPhone 6 with yellow case";
+		String location = "Bruxelles-Midi";
+		boolean pickedUp = false;
+		boolean archived = false;
+		
+		LostItem li1 = new LostItem(id, type, description, location, pickedUp, archived);
+		LostItem li2 = new LostItem(1, "phone", "iPhone 6 with yellow case", "Bruxelles-Midi", false, false);
+		
+		if(check == 1){
+			assertEquals(li1.hashCode(), li2.hashCode());
+		}
+		else if(check == 0){
+			li1.setId(1);
+			li1.setType("phone");
+			li1.setDescription("iPhone 6 with yellow case");
+			li1.setLocation("Vilvoorde");
+			li1.setPickedUp(true);
+			li1.setArchived(true);
+		}
 	}
 
 }
