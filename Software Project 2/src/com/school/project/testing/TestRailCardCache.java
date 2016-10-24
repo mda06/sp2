07e3c5a0049dd12e723ad3c76e388f827c4dbed5
Willem-Jan Pattyn <Willem-Jan Pattyn@192.168.1.6>
2016-10-24 17:35:41 +0200

    Added test case TestRailCardCache

---
+++ Software Project 2/src/com/school/project/testing/TestRailCardCache.java
@@ -0,0 +1,41 @@
+package com.school.project.testing;
+
+import static org.junit.Assert.assertEquals;
+
+import java.lang.reflect.Constructor;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.school.project.model.RailCard;
+import com.school.project.model.RailCardCache;
+
+public class TestRailCardCache {
+
+	int check = 1; //1 = true, 0 = false
+	
+	@Test
+	public void testInstance() {
+		
+		if(check == 1){			
+			assertEquals(RailCardCache.getInstance(), RailCardCache.getInstance());
+		}
+		else if(check == 0){
+			assertEquals(null, RailCardCache.getInstance());
+		}
+	}
+	
+	@Test
+	public void testAddTicket(){		
+		RailCard rc1 = new RailCard(1, "SCHOOL RAILCARD", "students under 26, discounted rate, unlimited travel on a fixed route", 16.10, 45.00, 161.00, true, false);
+		RailCardCache.getInstance().addTicket(rc1);
+		
+		if(check == 1){
+			assertEquals(rc1, RailCardCache.getInstance().getCache().get(0));
+		}
+		else if(check == 0){
+			RailCard rc2 = new RailCard(1, "JOURNEY RAILCARD", "commuters, unlimited travel on your route, emplyer's contribution", 26.00, 73.00, 260.00, true, false);
+			assertEquals(rc2, RailCardCache.getInstance().getCache().get(0));
+		}
+	}
+}
