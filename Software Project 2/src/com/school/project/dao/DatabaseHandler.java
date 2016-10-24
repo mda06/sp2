85ac208e5dffea6723fddbc27e911fc44d553fba
mda06 <michaelvm171@msn.com>
2016-10-24 12:14:10 +0200

    Added db/credentials for our credentials

--- Software Project 2/src/com/school/project/dao/DatabaseHandler.java
+++ Software Project 2/src/com/school/project/dao/DatabaseHandler.java
@@ -1,17 +1,43 @@
 package com.school.project.dao;
 
-import java.sql.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DatabaseHandler {
 	private java.sql.Connection connection;
 	private final String DB_URL = "dt5.ehb.be";
-	private final String DB_NAME = "";
-	private final String DB_USERNAME = "";
-	private final String DB_PASSWORD = "";
+	private final String DB_NAME;
+	private final String DB_USERNAME;
+	private final String DB_PASSWORD;
 	
 	private static DatabaseHandler instance;
 	
-	private DatabaseHandler(){};
+	private DatabaseHandler() {
+		String name = "", user = "", pass = "";
+		try (BufferedReader br = new BufferedReader(new FileReader("res/dbCredentials.txt"))) {
+
+			String line;
+			int i = 0;
+			while ((line = br.readLine()) != null) {
+				switch(++i) {
+				case 1 : name = line; break;
+				case 2 : user = line; break;
+				case 3 : pass = line; break;
+				}
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		DB_NAME = name;
+		DB_USERNAME = user;
+		DB_PASSWORD = pass;
+	}
 	
 	public static DatabaseHandler getInstance(){
 		if(instance == null)
