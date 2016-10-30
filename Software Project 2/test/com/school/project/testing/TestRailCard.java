9000ab4a78117add11dbcdf523e3644a996c6ffc
Willem-Jan Pattyn <Willem-Jan Pattyn@192.168.1.2>
2016-10-30 09:52:43 +0100

    Made TestRailCard less code duplicate

--- Software Project 2/test/com/school/project/testing/TestRailCard.java
+++ Software Project 2/test/com/school/project/testing/TestRailCard.java
@@ -8,7 +8,16 @@ import com.school.project.model.RailCard;
 
 public class TestRailCard {
 
-	int check = 1; //1 = true, 0 = false
+	int check = 0; //1 = true, 0 = false
+	
+	public static RailCard getFirstRailCard(){
+		return new RailCard(1, "SCHOOL RAILCARD", "students under 26, discounted rate, unlimited travel on a fixed route", 16.10, 45.00, 161.00, true, false);
+	}
+	
+	public static RailCard getSecondRailCard(){
+		return new RailCard(2, "JOURNEY RAILCARD", "commuters, unlimited travel on your route, emplyer's contribution", 26.00, 73.00, 260.00, true, false);
+	}
+	
 	@Test
 	public void testConstructor() {
 		int id = 1;
@@ -20,7 +29,7 @@ public class TestRailCard {
 		boolean hasFixedRoute = true;
 		boolean archived = false;
 		
-		RailCard rc1 = new RailCard(id, name, description, pricePerMonth, pricePer3Month, pricePerYear, hasFixedRoute, archived);
+		RailCard rc1 = TestRailCard.getFirstRailCard();
 		
 		if(check == 1){
 			assertEquals(id, rc1.getId());
@@ -33,7 +42,7 @@ public class TestRailCard {
 			assertEquals(archived, rc1.isArchived());
 		}
 		else if(check == 0){
-			rc1.setId(1);
+			rc1.setId(2);
 			rc1.setName("JOURNEY RAILCARD");
 			rc1.setDescription("commuters, unlimited travel on your route, emplyer's contribution");
 			rc1.setPricePerMonth(26.00);
@@ -55,46 +64,28 @@ public class TestRailCard {
 	
 	@Test
 	public void testEquals(){
-		int id = 1;
-		String name = "SCHOOL RAILCARD";
-		String description = "students under 26, discounted rate, unlimited travel on a fixed route";
-		double pricePerMonth = 16.10;
-		double pricePer3Month = 45.00;
-		double pricePerYear = 161.00; 
-		boolean hasFixedRoute = true;
-		boolean archived = false;
-		
-		RailCard rc1 = new RailCard(id, name, description, pricePerMonth, pricePer3Month, pricePerYear, hasFixedRoute, archived);
+		RailCard rc1 = TestRailCard.getFirstRailCard();
 		
 		if(check == 1){
-			RailCard rc2 = new RailCard(1, "SCHOOL RAILCARD", "students under 26, discounted rate, unlimited travel on a fixed route", 16.10, 45.00, 161.00, true, false);
+			RailCard rc2 = TestRailCard.getFirstRailCard();
 			assertEquals(true, rc1.equals(rc2));
 		}
 		else if(check == 0){
-			RailCard rc2 = new RailCard(1, "JOURNEY RAILCARD", "commuters, unlimited travel on your route, emplyer's contribution", 26.00, 73.00, 260.00, true, false);
+			RailCard rc2 = TestRailCard.getSecondRailCard();
 			assertEquals(true, rc1.equals(rc2));
 		}
 	}
 	
 	@Test
 	public void testHashCode(){
-		int id = 1;
-		String name = "SCHOOL RAILCARD";
-		String description = "students under 26, discounted rate, unlimited travel on a fixed route";
-		double pricePerMonth = 16.10;
-		double pricePer3Month = 45.00;
-		double pricePerYear = 161.00; 
-		boolean hasFixedRoute = true;
-		boolean archived = false;
-		
-		RailCard rc1 = new RailCard(id, name, description, pricePerMonth, pricePer3Month, pricePerYear, hasFixedRoute, archived);
+		RailCard rc1 = TestRailCard.getFirstRailCard();
 		
 		if(check == 1){
-			RailCard rc2 = new RailCard(1, "SCHOOL RAILCARD", "students under 26, discounted rate, unlimited travel on a fixed route", 16.10, 45.00, 161.00, true, false);
+			RailCard rc2 = TestRailCard.getFirstRailCard();
 			assertEquals(rc1.hashCode(), rc2.hashCode());
 		}
 		else if(check == 0){
-			RailCard rc2 = new RailCard(1, "JOURNEY RAILCARD", "commuters, unlimited travel on your route, emplyer's contribution", 26.00, 73.00, 260.00, true, false);
+			RailCard rc2 = TestRailCard.getSecondRailCard();
 			assertEquals(rc1.hashCode(), rc2.hashCode());
 		}
 	}
