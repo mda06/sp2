d975521eca5ba19cab8b8cad80a05bf7a4dc230f
Ruben <Ruben@DESKTOP-0P5QE42.home>
2016-11-01 17:03:53 +0100

    eerste stap naar werkende user
    -Checkbox voor Keuze om account aan te maken
    -save knop
    -fouten in languages rechtgezet
    -nota: userview heeft heel louche getters en setters

--- Software Project 2/src/com/school/project/gui/controller/UserController.java
+++ Software Project 2/src/com/school/project/gui/controller/UserController.java
@@ -1,17 +1,32 @@
 package com.school.project.gui.controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Observable;
 
 import javax.swing.border.TitledBorder;
 
+import com.school.project.gui.controller.runnable.RouteConnectionRunnable;
 import com.school.project.gui.view.UserView;
 import com.school.project.language.LanguageHandler;
 import com.school.project.language.LanguageObservable;
 
 public class UserController extends BaseController<UserView> {
-
+	
+	Boolean useCred = false;
 	public UserController() {
 		super(new UserView());
+		view.getpnlCredentials().setVisible(useCred);
+		initOptions();
+	}
+	
+	public void initOptions(){
+		view.getCBUseCredentials().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				useCred = view.getCBUseCredentials().getModel().isSelected();
+				view.getpnlCredentials().setVisible(useCred);
+			}
+		});
 	}
 
 	
@@ -27,6 +42,11 @@ public class UserController extends BaseController<UserView> {
 			view.getLbStreetLine2().setText(lh.getString("streetLine2"));
 			view.getLbZipcode().setText(lh.getString("zipcode"));
 			view.getLbCity().setText(lh.getString("city"));
+			view.getCBUseCredentials().setText(lh.getString("makeAccount"));
+			view.getBtnComplete().setText(lh.getString("save"));
+			
+			((TitledBorder)view.getPnlOptions().getBorder()).setTitle(lh.getString("options"));
+			view.getPnlOptions().repaint();
 			((TitledBorder)view.getpnlAccount().getBorder()).setTitle(lh.getString("account"));
 			view.getpnlAccount().repaint();
 			((TitledBorder)view.getpnlCredentials().getBorder()).setTitle(lh.getString("credentials"));
