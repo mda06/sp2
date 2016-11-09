5b539d7c6f094adaf6b3dfa9e6aaea30bf1a416e
mda06 <michaelvm171@msn.com>
2016-11-09 19:14:46 +0100

    Finish work on details of connections
    -Better columns name
    -Better way to display info

--- Software Project 2/src/com/school/project/gui/controller/ConnectionDetailsController.java
+++ Software Project 2/src/com/school/project/gui/controller/ConnectionDetailsController.java
@@ -1,9 +1,12 @@
 package com.school.project.gui.controller;
 
+import javax.swing.table.TableColumnModel;
+
 import com.school.project.gui.model.ConnectionDetailsTableModel;
 import com.school.project.gui.view.ConnectionDetailsFrame;
 import com.school.project.nmbs.model.Connection;
 import com.school.project.nmbs.model.Via;
+import com.school.project.util.DateUtil;
 
 public class ConnectionDetailsController {
 	private ConnectionDetailsFrame frame;
@@ -11,9 +14,23 @@ public class ConnectionDetailsController {
 	
 	public ConnectionDetailsController() {
 		frame = new ConnectionDetailsFrame();
-		tblModel = new ConnectionDetailsTableModel();
 		
+		initTablePreferences();
+	}
+	
+	private void initTablePreferences(){
+		tblModel = new ConnectionDetailsTableModel();
 		frame.getTblDetails().setModel(tblModel);
+		TableColumnModel tcm = frame.getTblDetails().getColumnModel();
+		
+		tcm.getColumn(tblModel.COLUMN_TRAIN_DIR).setPreferredWidth(300);
+		tcm.getColumn(tblModel.COLUMN_STEP_OFF).setPreferredWidth(300);
+		tcm.getColumn(tblModel.COLUMN_ARRIVAL_PLATFORM).setPreferredWidth(50);
+		tcm.getColumn(tblModel.COLUMN_ARRIVAL_DELAY).setPreferredWidth(50);
+		tcm.getColumn(tblModel.COLUMN_ARRIVAL_TIME).setPreferredWidth(200);
+		tcm.getColumn(tblModel.COLUMN_NEXT_PLATFORM).setPreferredWidth(50);
+		tcm.getColumn(tblModel.COLUMN_NEXT_DELAY).setPreferredWidth(50);
+		tcm.getColumn(tblModel.COLUMN_NEXT_TIME).setPreferredWidth(200);
 	}
 	
 	public void showConnection(Connection c) {
@@ -24,8 +41,16 @@ public class ConnectionDetailsController {
 			tblModel.addVia(v);
 		
 		frame.setVisible(true);
-		frame.getTxtArrival().setText(c.getArrival().getToStation().getName());
-		frame.getTxtDeparture().setText(c.getDeparture().getToStation().getName());
+		String str = c.getArrival().getToStation().getName();
+		str += " on " + DateUtil.timeStampToDate(c.getArrival().getDepartureInfo().getTimeStamp());
+		str += " at platform " + c.getArrival().getDepartureInfo().getPlatform().getName();
+		str += " with " + c.getArrival().getDepartureInfo().getDelay() + " delay";
+		frame.getTxtArrival().setText(str);
+		str = c.getDeparture().getToStation().getName();
+		str += " on " + DateUtil.timeStampToDate(c.getDeparture().getDepartureInfo().getTimeStamp());
+		str += " at platform " + c.getDeparture().getDepartureInfo().getPlatform().getName();
+		str += " with " + c.getDeparture().getDepartureInfo().getDelay() + " delay";
+		frame.getTxtDeparture().setText(str);
 		int min = (int) c.getDurationInMinutes();
 		int hour = min / 60;
 		min %= 60;
