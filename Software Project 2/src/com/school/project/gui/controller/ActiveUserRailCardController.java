feef05b99624e74eeedea5492ece1d16d30a9052
naits <naits@192.168.0.101>
2016-11-14 16:40:46 +0100

    Created ActiveUserRailCardView + ActiveRailCardTableModel + ActiveRailCardView and added ActiveUserRailCardView to MainFactory

--- Software Project 2/src/com/school/project/gui/controller/ActiveUserRailCardController.java
+++ Software Project 2/src/com/school/project/gui/controller/ActiveUserRailCardController.java
@@ -1,5 +1,27 @@
 package com.school.project.gui.controller;
 
-public class ActiveUserRailCardController {
+import java.util.Observable;
 
+import com.school.project.gui.model.ActiveRailCardTableModel;
+import com.school.project.gui.view.ActiveRailCardView;
+
+public class ActiveUserRailCardController extends BaseController<ActiveRailCardView>{
+	private ActiveRailCardTableModel tableModel;
+	private String strErrorAdd, strRemove;
+	
+	public ActiveUserRailCardController(){
+		super(new ActiveRailCardView());
+		tableModel = new ActiveRailCardTableModel();
+		view.getTable().setModel(tableModel);
+		view.getTable().setAutoCreateRowSorter(true);
+		
+		
+	}
+
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
--- Software Project 2/src/com/school/project/gui/view/ActiveRailCardView.java
+++ Software Project 2/src/com/school/project/gui/view/ActiveRailCardView.java
@@ -3,6 +3,7 @@ package com.school.project.gui.view;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.SpringLayout;
 
@@ -10,9 +11,8 @@ public class ActiveRailCardView extends BaseView {
 	private static final long serialVersionUID = 1L;
 	public final String KEY_BTNS = "buttons", KEY_PAY = "payment";
 	private JButton btnSelectUser;
-	private JLabel lblSelectUser;
+	private JLabel lblSelectUser, lblUser;
 	private JTable table;
-	private JPanel pnlTop;
 	
 	
 	
@@ -22,13 +22,60 @@ public class ActiveRailCardView extends BaseView {
 	}
 
 	private void initLayout(){
-		
-		btnSelectUser = new JButton();
-		lblSelectUser = new JLabel();
-		table = new JTable();
 		SpringLayout sp = new SpringLayout();
-		pnlTop = new JPanel(sp);
-		pnlTop.add(btnSelectUser);
-		pnlTop.add(lblSelectUser);
+		setLayout(sp);
+		
+		btnSelectUser = new JButton("Select user");
+		lblSelectUser = new JLabel("Selected user:");
+		lblUser = new JLabel("user");
+		
+		add(btnSelectUser);
+		add(lblSelectUser);
+		add(lblUser);
+		
+		table = new JTable();
+		JScrollPane scroll = new JScrollPane(table);
+		
+		add(scroll);
+		
+		sp.putConstraint(SpringLayout.WEST, btnSelectUser, 50, SpringLayout.WEST, this);
+		sp.putConstraint(SpringLayout.NORTH, btnSelectUser, 30, SpringLayout.NORTH, this);
+		sp.putConstraint(SpringLayout.SOUTH, btnSelectUser, 70, SpringLayout.NORTH, this);
+		sp.putConstraint(SpringLayout.EAST, btnSelectUser, 150, SpringLayout.WEST, btnSelectUser);
+
+		sp.putConstraint(SpringLayout.WEST, lblSelectUser, 50, SpringLayout.EAST, btnSelectUser);
+		sp.putConstraint(SpringLayout.NORTH, lblSelectUser, 20, SpringLayout.NORTH, this);
+		sp.putConstraint(SpringLayout.SOUTH, lblSelectUser, 50, SpringLayout.NORTH, lblSelectUser);
+		sp.putConstraint(SpringLayout.EAST, lblSelectUser, 140, SpringLayout.WEST, lblSelectUser);
+		
+		sp.putConstraint(SpringLayout.NORTH, lblUser, 20, SpringLayout.NORTH, this);
+		sp.putConstraint(SpringLayout.WEST, lblUser, 20, SpringLayout.EAST, lblSelectUser);
+		sp.putConstraint(SpringLayout.EAST, lblUser, 100, SpringLayout.WEST, lblUser);
+		sp.putConstraint(SpringLayout.SOUTH, lblUser, 70, SpringLayout.NORTH, this);
+		
+		sp.putConstraint(SpringLayout.NORTH, scroll, 150, SpringLayout.NORTH, this);
+		sp.putConstraint(SpringLayout.WEST, scroll, 30, SpringLayout.WEST, this);
+		sp.putConstraint(SpringLayout.EAST, scroll, -30, SpringLayout.EAST, this);
+		sp.putConstraint(SpringLayout.SOUTH, scroll, -30, SpringLayout.SOUTH, this);
+		
 	}
+
+	public JButton getBtnSelectUser() {
+		return btnSelectUser;
+	}
+
+	public JLabel getLblSelectUser() {
+		return lblSelectUser;
+	}
+
+	public JLabel getLblUser() {
+		return lblUser;
+	}
+
+	public JTable getTable() {
+		return table;
+	}
+
+	
+	
 }
