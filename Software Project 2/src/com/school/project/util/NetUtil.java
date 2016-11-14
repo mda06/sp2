3e729d40d60f611f565c32a31377b9b954d8df94
mda06 <michaelvm171@msn.com>
2016-11-14 12:12:37 +0100

    Added datepicker to route and add a new function to curl a url

--- Software Project 2/src/com/school/project/util/NetUtil.java
+++ Software Project 2/src/com/school/project/util/NetUtil.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.URL;
 
 public class NetUtil {
 	public static String curlURL(String url) throws IOException {
@@ -17,5 +18,20 @@ public class NetUtil {
 			}
 			return sb.toString();
 		}
+	}
+
+	/**
+	 * @depreciated 
+	 * Doesn't work with Station, but well with the rest
+	 * */
+	public static String jsonFromURL(String url) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		URL yahoo = new URL(url);
+		BufferedReader in = new BufferedReader(new InputStreamReader(yahoo.openStream()));
+		String inputLine;
+		while ((inputLine = in.readLine()) != null)
+			sb.append(inputLine);
+		in.close();
+		return sb.toString();
 	}
 }
