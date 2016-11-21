273752cea63d4b5386775c723067122cdac99008
mda06 <michaelvm171@msn.com>
2016-11-21 11:20:21 +0100

    Made base for the settings

---
+++ Software Project 2/src/com/school/project/gui/view/settings/RailCardSettingsView.java
@@ -0,0 +1,14 @@
+package com.school.project.gui.view.settings;
+
+import java.awt.Color;
+
+import com.school.project.gui.view.BaseView;
+
+public class RailCardSettingsView extends BaseView{
+	private static final long serialVersionUID = 1L;
+
+	public RailCardSettingsView() {
+		super("railcardsSettings");
+		setBackground(Color.YELLOW);
+	}
+}
