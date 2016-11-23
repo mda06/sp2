27bf8f841d1df9b5a76d3c8bcda85d9099cc1fc9
mda06 <michaelvm171@msn.com>
2016-11-23 08:47:55 +0100

    Charts update now dynamically

--- Software Project 2/src/com/school/project/gui/controller/statistics/StatisticsController.java
+++ Software Project 2/src/com/school/project/gui/controller/statistics/StatisticsController.java
@@ -2,7 +2,13 @@ package com.school.project.gui.controller.statistics;
 
 import java.util.Observable;
 
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.ui.RectangleInsets;
+
 import com.school.project.gui.controller.BaseController;
+import com.school.project.gui.model.statistics.AbstractChartModel;
+import com.school.project.gui.model.statistics.data.TicketSaleByUsersStatistic;
 import com.school.project.gui.view.statistics.StatisticsView;
 
 public class StatisticsController extends BaseController<StatisticsView> {
@@ -15,8 +21,25 @@ public class StatisticsController extends BaseController<StatisticsView> {
 	public void update(Observable o, Object arg) {
 	}
 	
+	private void updateDataset() {
+		new Thread(() -> {
+			view.getTabbedPane().removeAll();
+			view.getTabbedPane().add("Ticket Sales", createChartModel(new TicketSaleByUsersStatistic()));
+			view.repaint();
+		}).start();
+	}
+	
+	private ChartPanel createChartModel(AbstractChartModel model) {
+		JFreeChart chart = model.createChart();
+		chart.setPadding(new RectangleInsets());
+		ChartPanel c = new ChartPanel(chart);
+		c.setMouseWheelEnabled(true);
+		c.setMouseZoomable(true);
+		return c;
+	}
+	
 	@Override
 	public void show() {
-		view.updateDataset();
+		updateDataset();
 	}
 }
