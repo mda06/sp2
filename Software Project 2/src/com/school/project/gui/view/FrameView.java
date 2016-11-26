8a9d6fc82d14fc1265985c6cb5535a6fad2010bf
mda06 <michaelvm171@msn.com>
2016-11-26 15:45:14 +0100

    Added a language settings where you can add new languages
    TODO(see trello): 
    -Save this in the xml
    -Manually change this

--- Software Project 2/src/com/school/project/gui/view/FrameView.java
+++ Software Project 2/src/com/school/project/gui/view/FrameView.java
@@ -6,7 +6,6 @@ import java.awt.GridLayout;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JSplitPane;
 
@@ -16,7 +15,6 @@ public class FrameView extends JFrame{
 	private JSplitPane split;
 	private JMenuBar menuBar;
 	private JMenu menuOptions;
-	private JMenuItem miEn, miNl, miFr;
 	
 	public FrameView() {
 		setTitle("FrameView");
@@ -39,12 +37,6 @@ public class FrameView extends JFrame{
 	private void initMenu(){
 		menuBar = new JMenuBar();
 		menuOptions = new JMenu("Options");
-		miEn = new JMenuItem("English");
-		miFr = new JMenuItem("Francais");
-		miNl = new JMenuItem("Nederlands");
-		menuOptions.add(miNl);
-		menuOptions.add(miFr);
-		menuOptions.add(miEn);
 		menuBar.add(menuOptions);
 		this.setJMenuBar(menuBar);
 	}
@@ -60,34 +52,4 @@ public class FrameView extends JFrame{
 	public JMenu getMenuOptions() {
 		return menuOptions;
 	}
-
-	public void setMenuOptions(JMenu menuOptions) {
-		this.menuOptions = menuOptions;
-	}
-
-	public JMenuItem getMiEn() {
-		return miEn;
-	}
-
-	public void setMiEn(JMenuItem miEn) {
-		this.miEn = miEn;
-	}
-
-	public JMenuItem getMiNl() {
-		return miNl;
-	}
-
-	public void setMiNl(JMenuItem miNl) {
-		this.miNl = miNl;
-	}
-
-	public JMenuItem getMiFr() {
-		return miFr;
-	}
-
-	public void setMiFr(JMenuItem miFr) {
-		this.miFr = miFr;
-	}
-	
-	
 }
