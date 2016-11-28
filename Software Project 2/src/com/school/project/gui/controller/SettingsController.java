f77c3396eb8455a64e1c93f6d8567e308c9df815
SamoyP <pietersamoy@10.3.210.116>
2016-11-28 10:37:59 +0100

    de tabbladen van settings proberen vertalen

--- Software Project 2/src/com/school/project/gui/controller/SettingsController.java
+++ Software Project 2/src/com/school/project/gui/controller/SettingsController.java
@@ -7,24 +7,25 @@ import com.school.project.gui.controller.settings.LayoutSettingsController;
 import com.school.project.gui.controller.settings.RailCardSettingsController;
 import com.school.project.gui.controller.settings.TicketSettingsController;
 import com.school.project.gui.view.SettingsView;
+import com.school.project.language.LanguageHandler;
 import com.school.project.language.LanguageObservable;
 
-public class SettingsController extends BaseController<SettingsView>{
+public class SettingsController extends BaseController<SettingsView> {
 	private LanguageObservable observable;
-	
+
 	public SettingsController(LanguageObservable obs) {
 		super(new SettingsView());
 		observable = obs;
 		initSettings();
 	}
-	
+
 	private void initSettings() {
 		addSetting(new TicketSettingsController());
 		addSetting(new RailCardSettingsController());
 		addSetting(new LayoutSettingsController());
 		addSetting(new LanguageSettingsController(observable.getLanguageHandler()));
 	}
-	
+
 	private void addSetting(BaseController<?> bc) {
 		view.getTabbedPane().add(bc.getBaseView().CARD_KEY, bc.getBaseView());
 		observable.addObserver(bc);
@@ -32,5 +33,9 @@ public class SettingsController extends BaseController<SettingsView>{
 
 	@Override
 	public void update(Observable o, Object arg) {
+		if (o instanceof LanguageObservable) {
+			LanguageHandler lh = ((LanguageObservable) o).getLanguageHandler();
+			view.getTabbedPane();
+		}
 	}
 }
