67e8f2fd23847ea6cf7b80695ad14f7da79fce91
SamoyP <pieter.samoy@student.ehb.be>
2016-12-15 01:12:22 +0100

    Add files via upload
    bij deze. de vooruitgang met de raily IOS app. nog niet veel maar je kan al gegevens uit de api uitlezen en heb al wat layout gedaan.

---
+++ IOS App/Raily/Raily/ProfileViewController.swift
@@ -0,0 +1,17 @@
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("profile loaded")
+       
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
---
+++ IOS App/Raily/Raily/apiConnection.swift
@@ -0,0 +1,15 @@
+//
+//  apiConnection.swift
+//  Raily
+//
+//  Created by Pieter Samoy on 15/12/16.
+//  Copyright © 2016 com.pieter. All rights reserved.
+//
+
+import UIKit
+
+
+class apiConnection: UICollectionViewController {
+
+  
+}
