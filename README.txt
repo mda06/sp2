306228ac650e2e17f79eb7a12c265c2b1d366caa
SamoyP <pietersamoy@pieters-mbp-2.home>
2016-12-18 21:40:11 +0100

    creating new branch

---
+++ README.txt
@@ -0,0 +1,1 @@
+This is our Android Application for Software Project II
---
+++ app/src/main/java/com/school/mda/softwareproject2androidapp/HomeActivity.java
@@ -0,0 +1,14 @@
+package com.school.mda.softwareproject2androidapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        //Test comment for github...
+    }
+}
